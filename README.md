3cc5a807b4c779b1b8f9783244ef3df462e9ef14
hubtwork <hubtwork@gmail.com>
2023-12-13 14:07:12 +0900

    init project with terminus, pnpm

---
+++ README.md
@@ -0,0 +1,9 @@
+# NestJS base project with CI/CD on GithubActions Example
+
+For HangeHae-Plus students, nestjs project ci / cd example with github actions.
+
+### ROADMAP
+- [x] CI Piepeline
+- [ ] CD Piepeline
+- [ ] Pipeline Alert with Github / Slack
+- [ ] Logging configurations for CloudWatch
---
+++ src/support/health/health.module.ts
@@ -0,0 +1,12 @@
+import { HttpModule } from "@nestjs/axios"
+import { Module } from "@nestjs/common"
+import { TerminusModule } from '@nestjs/terminus'
+import { HealthCheckController } from "./health.controller"
+
+@Module({
+    imports: [
+        TerminusModule, HttpModule
+    ],
+    controllers: [HealthCheckController],
+})
+export class HealthModule {}
