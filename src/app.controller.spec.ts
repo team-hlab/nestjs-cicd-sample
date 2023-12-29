bf39a60a5516cdb17eb4623224bbd6a5e168ad9d
alen.heo <hubtwork@gmail.com>
2023-12-29 17:44:24 +0900

    [fix] test

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,10 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+
+describe('[SpecTest]', () => {
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+    });
+  });
+});
