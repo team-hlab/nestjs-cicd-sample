920a42d07b354a850f4ae66f0bd25cd0984574f7
alen-heo <58537670+hubtwork@users.noreply.github.com>
2023-12-29 12:12:02 +0900

    Revert "[sync] release-0.0.2 to main (#8)"
    This reverts commit f63c624228117bc8b940b024036fb5b8ffc6b59e.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './support/health/health.module';
-import { ShowcaseModule } from './module/showcase/showcase.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-    }),
-
-    HealthModule,
-    ShowcaseModule,
-  ],
+  imports: [HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/support/health/health.controller.ts
+++ src/support/health/health.controller.ts
@@ -1,22 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import {
-  HealthCheck,
-  HealthCheckService,
-  HttpHealthIndicator,
-} from '@nestjs/terminus';
+import { Controller, Get } from "@nestjs/common";
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from "@nestjs/terminus";
 
 @Controller('health')
 export class HealthCheckController {
-  constructor(
-    private service: HealthCheckService,
-    private http: HttpHealthIndicator,
-  ) {}
+    constructor(
+        private service: HealthCheckService,
+        private http: HttpHealthIndicator,
+    ) {}
 
-  @Get()
-  @HealthCheck()
-  healthCheck() {
-    return this.service.check([
-      () => this.http.pingCheck('me', 'http://localhost:3000'),
-    ]);
-  }
+    @Get()
+    @HealthCheck()
+    healthCheck() {
+        return this.service.check([
+            () => this.http.pingCheck('me', 'http://localhost:3000')
+        ])
+    }
 }
